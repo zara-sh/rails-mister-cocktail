138a1521ebd1913290c2db17a635dea7f46bdd4c
zara-sh <zara.regist@gmail.com>
2017-10-13 09:32:57 +0900

    Add figaro - Protect my secret data in config/application.yml

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 ruby '2.3.4'
+gem 'figaro'
+gem 'cloudinary'
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
@@ -6,7 +8,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+gem "simple_form"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.6'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  # get 'cocktails/index'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/create'
+
+  # get 'doses/new'
+
+  # get 'doses/create'
+
+  # get 'doses/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create,] do
+    resources :doses, only:[:new, :create]
+  end
+ resources :doses, only:[:destroy]
 end
